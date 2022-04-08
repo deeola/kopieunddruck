6ce71d0056ac5f7630da85f10be22cc5ab75e8ef
Adeola Bamigboye <Adeola.bamigboye@prisma-capacity.eu>
2022-04-08 09:22:27 +0200

    style binding and printing

--- src/components/Services/Binding.js
+++ src/components/Services/Binding.js
@@ -1,46 +1,105 @@
 import React from "react";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
+import binding from "../../assest/binding.jpg";
+import styled from "styled-components";
 
+const BodyContainer = styled.div``;
+
+const Header = styled.h1`
+  margin: 20px auto 20px 0px;
+  width: 60%;
+  background-color: #adc906;
+  color: #eaeef1;
+  padding: 5px 0px 10px 30px;
+`;
+
+const ImageContainer = styled.img`
+  width: 100%;
+`;
+
+const ImageDiv = styled.div``;
+
+const Preamble = styled.p`
+  width: 90%;
+  margin: auto;
+  font-size: 15px;
+  text-align: left;
+  line-height: 25px;
+`;
+
+const Bildungs = styled.div`
+  width: 90%;
+  margin: auto;
+`;
+
+const BindungContainer = styled.div`
+  margin-bottom: 10px;
+`;
+
+const BindungTitle = styled.h5`
+  margin: 10px 0px;
+  color: #adc906;
+  font-weight: 900;
+`;
+
+const BindungText = styled.p`
+  font-size: 15px;
+  text-align: left;
+  line-height: 25px;
+`;
+
+const BindungAsterik = styled.p`
+  width: 90%;
+  margin: auto;
+  background-color: #adc906;
+  color: #eaeef1;
+  padding: 10px;
+`;
 
 const Binding = () => {
   return (
     <div>
       <Navbar />
-      <div>
-        <h1>Binding</h1>
-        <p>
-          Wohin mit losen Blattsammlungen? Oder: die Bachelorarbeit ist
+      <BodyContainer>
+        <ImageDiv>
+          <ImageContainer src={binding} alt="printing" />
+        </ImageDiv>
+        <Header>Bindungen</Header>
+        <Preamble>
+          Wohin mit losen Blattsammlungen? Oder die Bachelorarbeit ist
           geschrieben und gedruckt und was hält das Werk nun zusammen? Natürlich
           eine der verschiedenen Bindemöglichkeiten die ihr bei uns erhaltet:
-        </p>
-        <div>
-          <div>
-            <h5>Thermobindung*</h5>
-            <p>
+        </Preamble>
+        <Bildungs>
+          <BindungContainer>
+            <BindungTitle>Thermobindung*</BindungTitle>
+            <BindungText>
               VipCover - fester Bucheinband der auf der Titelseite mit einer
               Gold- oder Silberprägung versehen werden kann SteelCover -
               flexibler Bucheinband der aus einem lederartigen Rücken mit
               transparenter Vorder- und Rückfolie besteht.
-            </p>
-          </div>
-          <div>
-            <h5>Wire-O-Bindungen*</h5>
-            <p>
+            </BindungText>
+          </BindungContainer>
+          <BindungContainer>
+            <BindungTitle>Wire-O-Bindungen*</BindungTitle>
+            <BindungText>
               Drahtspiralbindung - sie hat den Vorteil, dass sie komplett plan
               aufgeschlagen werden kann und auch bei hoher Beanspruchung über
               eine sehr gute Haltbarkeit verfügt.
-            </p>
-          </div>
-          <div>
-            <h5>Kalenderbindung*</h5>
-            <p>
-            Die Drahtspiralbindung mit Kalenderaufhängung, inkl. Daumenstanzung, sorgt für komfortables Umblättern.
-            </p>
-          </div>
-        </div>
-        <p>* in verschiedenen Farben erhältlich</p>
-      </div>
+            </BindungText>
+          </BindungContainer>
+          <BindungContainer>
+            <BindungTitle>Kalenderbindung*</BindungTitle>
+            <BindungText>
+              Die Drahtspiralbindung mit Kalenderaufhängung, inkl.
+              Daumenstanzung, sorgt für komfortables Umblättern.
+            </BindungText>
+          </BindungContainer>
+        </Bildungs>
+
+        <BindungAsterik>* in verschiedenen Farben erhältlich</BindungAsterik>
+      </BodyContainer>
       <Footer />
     </div>
   );
--- src/components/Services/Printing.js
+++ src/components/Services/Printing.js
@@ -1,24 +1,64 @@
 import React from "react";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
+import styled from "styled-components";
+import druck from "../../assest/druck.jpg";
+
+const BodyContainer = styled.div``;
+
+const Header = styled.h1`
+  margin: 20px auto 20px 0px;
+  
+  width: 50%;
+  background-color: #adc906;
+  color:#eaeef1 ;
+  padding-left: 30px;
+`;
+
+const ImageContainer = styled.img`
+  width: 100%;
+
+`;
+
+const ImageDiv = styled.div``;
+
+const UnorderedList = styled.ul`
+background-color: #eaeef1;
+padding: 20px 40px;
+width: 96%;
+margin: 10px auto auto auto;
+border-radius: 20px;
+
+li{
+  list-style: square;
+
+  margin-bottom: 10px;
+ 
+}
+`
 
 const Printing = () => {
   return (
     <div>
       <Navbar />
-      <div>
-        <h1>Drucken</h1>
-        <div>
-          <ul>
-            <li>sofort zum Mitnehmen</li>
-            <li>farbig und schwarzweiß</li>
-            <li>brillante Ergebnisse (abhängig von der Vorlage)</li>
-            <li>Standard DIN-Formate A6, A5, A4, A3, A3+, A2, A1, A0</li>
-            <li>Sonderformate mit extremen Überlängen bis hin zu Meterware</li>
-            <li>individuelle Auflagenstärken (ab 1 Stück bis hin zur Tausenderauflage)</li>
-          </ul>
-        </div>
-      </div>
+      <BodyContainer>
+        <ImageDiv>
+          <ImageContainer src={druck} alt="printing" />
+        </ImageDiv>
+        <Header>Drucken</Header>
+
+        <UnorderedList>
+          <li>Sofort zum Mitnehmen.</li>
+          <li>Farbig und schwarzweiß.</li>
+          <li>Brillante Ergebnisse (abhängig von der Vorlage).</li>
+          <li>Standard DIN-Formate A6, A5, A4, A3, A3+, A2, A1, A0.</li>
+          <li>Sonderformate mit extremen Überlängen bis hin zu Meterware.</li>
+          <li>
+            Individuelle Auflagenstärken (ab 1 Stück bis hin zur
+            Tausenderauflage).
+          </li>
+        </UnorderedList>
+      </BodyContainer>
       <Footer />
     </div>
   );
