1096bc9dc207aa7f96de9b3a8dc0430892a5b403
Adeola Bamigboye <Adeola.bamigboye@prisma-capacity.eu>
2022-04-09 21:55:46 +0200

    add google maps

--- src/components/Home/Map.js
+++ src/components/Home/Map.js
@@ -1,23 +1,55 @@
-import styled from 'styled-components'
-import React from 'react';
-import Maps from '../../assest/map.png';
-
-const MapImage = styled.img`
-width: 100%;
-`
+import styled from "styled-components";
+import React from "react";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from "react-google-maps";
 
 const MapContainer = styled.div`
+  width: 96%;
+  height: 400px;
+  margin: 20px auto auto auto;
+`;
 
-width: 96%;
+function MyMap() {
+  //Lat and Lng values to be used in position
+  const BerlinDessau = [
+    
+    {
+      lat: 51.8364815,
+      lng: 12.2441451,
+      parkId: 2,
+      name: "dessau",
+    },
+  ];
 
-margin: 20px auto auto auto;
-`
-const Map = () => {
   return (
-    <MapContainer>
-        <MapImage src={Maps}  alt='fakemap' />
+    <GoogleMap
+      defaultZoom={16}
+      defaultCenter={{ lat: 51.8364815, lng: 12.2441451 }}
+    >
+      {BerlinDessau.map((city) => (
+        <Marker key={city.parkId} position={{ lat: city.lat, lng: city.lng }} />
+      ))}
+    </GoogleMap>
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(MyMap));
+
+function Map() {
+  return (
+    <MapContainer className="mapContainerDiv">
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${`AIzaSyBoWqoOHLWPQo0o4_K5aRo_rL39dsQBTsY`}`}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+      />
     </MapContainer>
-  )
+  );
 }
 
 export default Map;
